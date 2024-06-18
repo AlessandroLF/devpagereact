46761d14726641853fe63ab0a7d0a7e903740048
AlessandroLF <lombardoalessandro25@hotmail.com>
2024-06-18 01:08:03 -0600

    lil bug fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import ppic from './ProfPic.png';
 import {Experience, Bd, Animations, Info} from './components/basePanels'
 import plogo from './prgramIcon.png';
-import ilogo from './iLogo.png'
 
 function App() {
 
@@ -55,7 +54,7 @@ function App() {
           <div className={active.bd ? 'btn on' : 'btn'} id='bd' onClick={onClick} ><img src='https://cdn-icons-png.flaticon.com/512/15156/15156593.png' alt='Git' onClick={onImgClick}/></div>
           {/*<div className={active.an ? 'btn on' : 'btn'} id='an' onClick={onClick} ><img src='https://media.tenor.com/EJQvxUMk__MAAAAi/runner.gif' alt='Animations' onClick={onImgClick}/></div>*/}
           <div className={active.an ? 'btn on' : 'btn'} id='an' onClick={onClick} ><img src='https://cdn-icons-png.flaticon.com/512/7187/7187174.png' alt='Animations' onClick={onImgClick}/></div>
-          <div className={active.io ? 'btn on' : 'btn'} id='io' onClick={onClick} ><img src={ilogo} alt='Info' onClick={onImgClick}/></div>
+          <div className={active.io ? 'btn on' : 'btn'} id='io' onClick={onClick} ><img src='https://cdn-icons-png.flaticon.com/512/1076/1076745.png' alt='Info' onClick={onImgClick}/></div>
         </div>
         {pannel}
       </div>
--- src/components/basePanels.js
+++ src/components/basePanels.js
@@ -145,7 +145,7 @@ export const Info = ()=>{
         <div className='panel v' >
             {modal && <div className='modal' onClick={onModalClick} >
                     <embed src='https://devpage-ojxi.onrender.com/CV_Alessandro_Lombardo_EN.pdf' type='application/pdf' />
-                    <div className='btn close'>X</div>
+                    <div className='btn close'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></div>
                 </div>
             }
             <table>
